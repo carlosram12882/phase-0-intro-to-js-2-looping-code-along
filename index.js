aae1dbed76389d2c9c0cca4c1865edb6e9e1de7d
Carlos Ramirez <carlosramirez686@gmail.com>
2021-12-23 18:00:29 -0800

    Assignment Completed

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Code your solutions in this file
+const thanksCards = ["Charlis", "Samip", "Ali"]
+
+function writeCards(thanksCards) {
+   const newArray =[];
+    for (let i = 0; i < thanksCards.length; i++) {
+      console.log(`Thank you, ${thanksCards[i]}, for the wonderful surprise gift!`);
+      newArray.push(`Thank you, ${thanksCards[i]}, for the wonderful surprise gift!`);
+      debugger;
+    }
+  
+    return newArray;
+  }
+  
+  writeCards(thanksCards);
+
+function countDown(){
+    let i = 10;
+    while (i > -1)
+    console.log(i--)
+
+}
+
+
+
+
+
+
+
+
+
